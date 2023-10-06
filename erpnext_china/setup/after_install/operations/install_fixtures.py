d8086178eb3de382ae3f1e1d2ca5d1dceda9ad39
saoxia <407469895@qq.com>
2023-10-06 15:02:31 +0800

    优化workspace替换方式

--- erpnext_china/setup/after_install/operations/install_fixtures.py
+++ erpnext_china/setup/after_install/operations/install_fixtures.py
@@ -42,20 +42,44 @@ def install(country='China'):
 #修改workspace文件
 def overwrite_workspace():
 	#替换文件
-	import shutil,json
-	source_path = Path(__file__).parent.parent / 'workspace' / 'crm' / 'crm.json'
-	target_path = (Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'crm' / 'workspace'  / 'crm' / 'crm.json')
-	shutil.copy2(source_path, target_path)
-	
-	#使文件生效
-	save_workspace_blocks(target_path)
+	import shutil
+	workspace_file_hooks = [
+		{'source':(Path(__file__).parent.parent / 'workspace' /'crm' / 'crm.json'),
+   		 'target':(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'crm' / 'workspace' / 'crm' / 'crm.json')}
+		,{'source':(Path(__file__).parent.parent / 'workspace' /'buying' / 'buying.json'),
+   		 'target':(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'buying' / 'workspace' / 'buying' / 'buying.json')}
+		,{'source':(Path(__file__).parent.parent / 'workspace' /'manufacturing' / 'manufacturing.json'),
+   		 'target':(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'manufacturing' / 'workspace' / 'manufacturing' / 'manufacturing.json')}
+		,{'source':(Path(__file__).parent.parent / 'workspace' /'selling' / 'selling.json'),
+   		 'target':(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'selling' / 'workspace' / 'selling' / 'selling.json')}
+		,{'source':(Path(__file__).parent.parent / 'workspace' /'stock' / 'stock.json'),
+   		 'target':(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'stock' / 'workspace' / 'stock' / 'stock.json')}
+		 ]
+	for i in workspace_file_hooks:
+		source_path = i['source']
+		target_path = i['target']
+		shutil.copy2(source_path, target_path)
+		#使文件生效
+		save_workspace_blocks(target_path)
 
 	#直接在源文件上修改
-	target_path2 = (Path(__file__).parent.parent.parent.parent.parent.parent / 'frappe' / 'frappe' / 'automation' / 'workspace'  / 'tools' / 'tools.json')
-	save_workspace_blocks(target_path2)
+	workspace_file_path =[(Path(__file__).parent.parent.parent.parent.parent.parent / 'frappe' / 'frappe' / 'automation' / 'workspace'  / 'tools' / 'tools.json')
+					   	 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'frappe' / 'frappe' / 'website' / 'workspace'  / 'website' / 'website.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'frappe' / 'frappe' / 'core' / 'workspace'  / 'build' / 'build.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'frappe' / 'frappe' / 'integrations' / 'workspace'  / 'integrations' / 'integrations.json')
+					     ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'accounts' / 'workspace'  / 'accounting' / 'accounting.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'assets' / 'workspace'  / 'assets' / 'assets.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'quality_management' / 'workspace'  / 'quality' / 'quality.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'projects' / 'workspace'  / 'projects' / 'projects.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'support' / 'workspace'  / 'support' / 'support.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'setup' / 'workspace'  / 'erpnext_settings' / 'erpnext_settings.json')
+						 ,(Path(__file__).parent.parent.parent.parent.parent.parent / 'erpnext' / 'erpnext' / 'erpnext_integrations' / 'workspace'  / 'erpnext_integrations' / 'erpnext_integrations.json')
+						 ]
+	for i in workspace_file_path:
+		save_workspace_blocks(i)
 
 def save_workspace_blocks(file_path):
-	import warnings
+	import warnings,json
 	with open(file_path, 'r') as file:
 		file_content = file.read()
 
