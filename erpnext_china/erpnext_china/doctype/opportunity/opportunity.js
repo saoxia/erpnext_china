196109e5d934678d050f2c42f811e13254d74fd2
saoxia <407469895@qq.com>
2023-10-14 15:51:21 +0800

    删除【商机来源】下拉选项中的意向客户

--- erpnext_china/erpnext_china/doctype/opportunity/opportunity.js
+++ erpnext_china/erpnext_china/doctype/opportunity/opportunity.js
@@ -1,6 +1,16 @@
 frappe.ui.form.on('Opportunity', {
 	refresh(frm) {
+	// 产出采购相关按钮
 	frm.remove_custom_button(__('Supplier Quotation'), __('Create'));
 	frm.remove_custom_button(__('Request For Quotation'), __('Create'));
+	// 删除【商机来源】下拉选项中的意向客户
+	frm.set_query("opportunity_from", function() {
+		return{
+			"filters": {
+				"name": ["in", ["Customer", "Lead"]]
+			}
+		}
+	});
+
 	}
 })
