312165df53b4795669d7571187caf92649ed60cd
saoxia <407469895@qq.com>
2023-10-14 14:49:10 +0800

    添加获取员工所有下级的函数

--- erpnext_china/erpnext_china/doctype/employee/employee.py
+++ erpnext_china/erpnext_china/doctype/employee/employee.py
@@ -229,44 +229,6 @@ class Employee(NestedSet):
 			frappe.cache().hdel("employees_with_number", cell_number)
 			frappe.cache().hdel("employees_with_number", prev_number)
 
-	
-	def get_employee_tree(self,parent=None, 
-						pluck='email',
-						orient='list',
-						levle=None,
-						is_root=None,
-						use_cache=False):
-		
-		'''
-        parent: default None
-            用户唯一标识的类型，可以输入str或dict
-            key: email|userid|username
-            value: 唯一标识的值
-
-        pluck: default ['email'] 返回的字段名
-            email|userid|username
-
-        orient: list|dict , 是否返回树状结构
-
-        levle: all|int ,返回多少层级的信息
-
-        is_root: False
-		'''
-		if is_root:
-			employee = 'HR-EMP-00002'
-		# 递归函数来获取下级employee
-		def get_subordinates(employee):
-			subordinates = []
-
-			filters = {'reports_to': employee,
-					'status': 'Active'}
-			employees = frappe.get_all('Employee',filters=filters,fields=['employee'],as_list=True)
-			employees = [item[0] for item in employees]
-			for i in employees:
-				subordinates.append(i)
-			subordinates.append(get_subordinates(i))
-			return subordinates
-		return get_subordinates(employee)
 
 def validate_employee_role(doc, method):
 	# called via User hook
@@ -468,3 +430,58 @@ def has_upload_permission(doc, ptype="read", user=None):
 		return True
 	return doc.user_id == user
 
+
+@frappe.whitelist()
+def get_employee_tree(parent=None, 
+					pluck='email',
+					orient='list',
+					levle=None,
+					is_root=None,
+					use_cache=False):
+	
+	'''
+	注意：io压力增加时添加从缓存中读取的代码
+
+	parent: default None
+		用户唯一标识的类型，可以输入str或dict
+		key: email|userid|username
+		value: 唯一标识的值
+
+	pluck: default 'email' 返回的字段名
+		email|userid|username
+
+	orient: list|dict , 是否返回树状结构
+
+	levle: all|int ,返回多少层级的信息
+
+	is_root: False
+	'''
+	if is_root:
+		# 树的最顶点
+		employee = 'HR-EMP-00002'
+	else:
+		employee = parent
+	# 递归函数来获取下级employee
+	def get_subordinates(employee):
+		subordinates = []
+
+		filters = {'reports_to': employee,
+				'status': 'Active'}
+		employees = frappe.get_all('Employee',filters=filters,fields=['employee'],as_list=True)
+		employees = [item[0] for item in employees]
+		for i in employees:
+			subordinates.append(i)
+			subordinates.append(get_subordinates(i))
+		return subordinates
+	
+	if orient == 'list':
+		subordinates = get_subordinates(employee)
+	
+	if (pluck == 'email') and (orient == 'list'):
+		# 返回email的列表
+		filters = {'employee': ['in',subordinates],
+				'status': 'Active'}
+		subordinates_email = frappe.get_all('Employee',filters=filters,fields=['user_id'],as_list=True)
+		subordinates= [item[0] for item in subordinates_email]
+		
+	return subordinates
