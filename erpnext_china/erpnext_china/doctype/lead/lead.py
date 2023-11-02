9e6e0b98df1e3076735df796e889b64fdd52f138
saoxia <407469895@qq.com>
2023-11-02 10:35:53 +0800

    线索权限修改为 线索负责人和被分配人

--- erpnext_china/erpnext_china/doctype/lead/lead.py
+++ erpnext_china/erpnext_china/doctype/lead/lead.py
@@ -474,7 +474,7 @@ def add_lead_to_prospect(lead, prospect):
 def lead_has_query_permission(user):
 	users = get_employee_tree(parent=user)
 	users.append(user)
-	users_str = str(tuple(users)).replace(',)',')')
+	users_str_tuple = str(tuple(users)).replace(',)',')')
 
 	if frappe.db.get_value('Has Role',{'parent':user,'role':'System Manager'}):
 		# 如果角色包含管理员，则看到全量
@@ -482,7 +482,7 @@ def lead_has_query_permission(user):
 	else:
 		# 其他情况则只能看到自己拥有的线索
 		# 待添加上级可以看到下级的线索
-		users = get_employee_tree(parent=user)
-		users = str(tuple(users))
-		conditions = f"owner in {users_str}" 
+		# 线索负责人、被分配者都可以看到线索
+		users_str_list = str(list(users)).replace("'",'"')
+		conditions = f"lead_owner in {users_str_tuple} or JSON_OVERLAPS(_assign,'{users_str_list}')"
 	return conditions
