8d5804668f80c7d67431d3c16135d4aacb24de78
digitwise <407469895@qq.com>
2023-11-10 18:40:27 +0800

    劳动合同修改为子表形式

---
+++ erpnext_china/erpnext_china/doctype/employee_contract/employee_contract.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Digitwise Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EmployeeContract(Document):
+	pass
