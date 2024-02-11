98846b756b13fcf33b5ea27e135adfa213b0cf08
digitwise <407469895@qq.com>
2024-02-12 00:32:55 +0800

    更新button权限配置

--- erpnext_china/public/js/form_button_permission.js
+++ erpnext_china/public/js/form_button_permission.js
@@ -1,21 +1,46 @@
-const MyForm = class MyForm extends frappe.ui.form.Form {
+const PermissionForm = class PermissionForm extends frappe.ui.form.Form {
 	add_custom_button(label, fn, group) {
-		console.log('------------')
-		// temp! old parameter used to be icon
-		if (group && group.indexOf("fa fa-") !== -1) group = null;
+		let doctype_name = this.doctype;
+		let groups = {
+			'创建':['更新项目']
+		};
+	
+		//let labels = groups[group];
+		let labels = [__('Update Items')];
+		
 
-		let btn = this.page.add_inner_button(label, fn, group);
 
-		if (btn) {
-			// Add actions as menu item in Mobile View
-			let menu_item_label = group ? `${group} > ${label}` : label;
-			let menu_item = this.page.add_menu_item(menu_item_label, fn, false);
-			menu_item.parent().addClass("hidden-xl");
+		frappe.call({
+			'method': "erpnext_china.api.get_button_permission",
+			'args': {
+				'doctype':doctype_name,
+				'label':__('Update Items')
+			},
+			'callback': function(r){
+				console.log(r.message)
+			}
+		
+		});
+		
 
-			this.custom_buttons[label] = btn;
+		if (labels.includes(label)) {
+		 	
+			// temp! old parameter used to be icon
+			if (group && group.indexOf("fa fa-") !== -1) group = null;
+
+			let btn = this.page.add_inner_button(label, fn, group);
+
+			if (btn) {
+				// Add actions as menu item in Mobile View
+				let menu_item_label = group ? `${group} > ${label}` : label;
+				let menu_item = this.page.add_menu_item(menu_item_label, fn, false);
+				menu_item.parent().addClass("hidden-xl");
+
+				this.custom_buttons[label] = btn;
+			}
+			return btn;
 		}
-		return btn;
 	}
 }
 
-frappe.ui.form.Form = MyForm
+frappe.ui.form.Form = PermissionForm
