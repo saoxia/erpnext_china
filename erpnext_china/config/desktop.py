f7c62f067d23cdcf37cc0a2b2674a4a17a86e341
digitwise <407469895@qq.com>
2024-02-25 10:05:05 +0800

    update

---
+++ erpnext_china/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "ERPNext China",
+			"type": "module",
+			"label": _("ERPNext China")
+		}
+	]
---
+++ erpnext_china/config/docs.py
@@ -0,0 +1,7 @@
+"""
+Configuration for docs
+"""
+
+
+def get_context(context):
+	context.brand_html = "ERPNext China"
