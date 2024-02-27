c1e1c0d22fa02d1cd55b2b3cda79aa840f69352d
digitwise <407469895@qq.com>
2024-02-27 22:54:44 +0800

    修复测试主题

--- erpnext_china/erpnext_china/overrides/user/user.py
+++ erpnext_china/erpnext_china/overrides/user/user.py
@@ -2,5 +2,5 @@ import frappe
 
 @frappe.whitelist()
 def switch_theme(theme):
-	if theme in ["Dark", "Light", "Automatic", "business"]:
+	if theme in ["Dark", "Light", "Automatic", "Business"]:
 		frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
