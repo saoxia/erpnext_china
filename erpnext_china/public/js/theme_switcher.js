ca7300281a928c5b97ebb063075f49ae8dd810c6
digitwise <407469895@qq.com>
2024-02-27 23:54:46 +0800

    更新主题的翻译

--- erpnext_china/public/js/theme_switcher.js
+++ erpnext_china/public/js/theme_switcher.js
@@ -10,22 +10,22 @@ frappe.ui.ThemeSwitcher = class CustomThemeSwitcher extends frappe.ui.ThemeSwitc
 			this.themes = [
 				{
 					name: "light",
-					label:("Frappe Light"),
+					label:__("Frappe Light"),
 					info:("Light Theme"),
 				},
 				{
 					name: "dark",
-					label:"Timeless Night",
+					label:__("Timeless Night"),
 					info:"Dark Theme",
 				},
 				{
 					name: "automatic",
-					label:"Automatic",
+					label:__("Automatic"),
 					info:"Uses system's theme to switch between light and dark mode",
 				},
                 {
                     name:"business",
-                    label: "Business",
+                    label: __("Business"),
                     info: "主题正在开发中"
                 }
 			];
