5bf883e7925c099cdaa4df12b1436c83a92d6456
digitwise <407469895@qq.com>
2024-02-28 19:54:10 +0800

    Update hooks.py

--- erpnext_china/hooks.py
+++ erpnext_china/hooks.py
@@ -31,9 +31,9 @@ doctype_js = {
     'Sales Order':'erpnext_china/custom_form_script/sales_order/sales_order.js',
 }
 permission_query_conditions = {
-    "Lead": "erpnext_china.erpnext_china.custom_form_script.lead.permission_lead.lead_has_query_permission",
-    "Quotation": "erpnext_china.erpnext_china.custom_form_script.quotation.permission_quotation.quotation_has_query_permission",
-    "Opportunity": "erpnext_china.erpnext_china.custom_form_script.opportunity.permission_opportunity.opportunity_has_query_permission",
+    "Lead": "erpnext_china.erpnext_china.custom_form_script.lead.permission_lead.has_query_permission",
+    "Quotation": "erpnext_china.erpnext_china.custom_form_script.quotation.permission_quotation.has_query_permission",
+    "Opportunity": "erpnext_china.erpnext_china.custom_form_script.opportunity.permission_opportunity.has_query_permission",
     "Sales Order": "erpnext_china.erpnext_china.custom_form_script.sales_order.permission_sales_order.has_query_permission", 
 }
 
