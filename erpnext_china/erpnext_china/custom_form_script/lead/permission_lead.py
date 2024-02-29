f49e265941ebe615593304339674625f6daffc41
digitwise <407469895@qq.com>
2024-02-29 11:32:02 +0800

    修复权限

--- erpnext_china/erpnext_china/custom_form_script/lead/permission_lead.py
+++ erpnext_china/erpnext_china/custom_form_script/lead/permission_lead.py
@@ -1,35 +1,27 @@
 import frappe
 
-
 from erpnext_china.hrms_china.custom_form_script.employee.employee import get_employee_tree
 
 def has_query_permission(user):
-	users = get_employee_tree(parent=user)
-	users.append(user)
-	users_str_tuple = str(tuple(users)).replace(',)',')')
-
 	if frappe.db.get_value('Has Role',{'parent':user,'role':'System Manager'}):
 		# 如果角色包含管理员，则看到全量
 		conditions = ''
 	else:
-		# 其他情况则只能看到自己拥有的线索
-		# 待添加上级可以看到下级的线索
-		# 线索负责人、被分配者都可以看到线索
-		users_str_list = str(list(users)).replace("'",'"')
-		conditions = f"lead_owner in {users_str_tuple} or JSON_OVERLAPS(_assign,'{users_str_list}')"
+		# 其他情况则只能看到自己,上级可以看到下级
+		users = get_employee_tree(parent=user)
+		users.append(user)
+		users_str = str(tuple(users)).replace(',)',')')
+		conditions = f"owner in {users_str}" 
 	return conditions
 
-def has_permission(doc, user=None, permission_type=None):
-	users = get_employee_tree(parent=user)
-	users.append(user)
-	users_str = str(tuple(users)).replace(',)',')')
-
+def has_permission(doc, user, permission_type=None):
 	if frappe.db.get_value('Has Role',{'parent':user,'role':['in',['System Manager']]}):
 		# 如果角色包含管理员，则看到全量
-		conditions = ''
 		return True
 	else:
+		# 其他情况则只能看到自己,上级可以看到下级
 		users = get_employee_tree(parent=user)
+		users.append(user)
 		if doc.owner in users:
 			return True
 		else:
--- erpnext_china/erpnext_china/custom_form_script/quotation/permission_quotation.py
+++ erpnext_china/erpnext_china/custom_form_script/quotation/permission_quotation.py
@@ -3,32 +3,25 @@ import frappe
 from erpnext_china.hrms_china.custom_form_script.employee.employee import get_employee_tree
 
 def has_query_permission(user):
-	users = get_employee_tree(parent=user)
-	users.append(user)
-	users_str = str(tuple(users)).replace(',)',')')
-
 	if frappe.db.get_value('Has Role',{'parent':user,'role':'System Manager'}):
 		# 如果角色包含管理员，则看到全量
 		conditions = ''
 	else:
-		# 其他情况则只能看到自己拥有的报价
-		# 待添加上级可以看到下级的报价
+		# 其他情况则只能看到自己,上级可以看到下级
 		users = get_employee_tree(parent=user)
-		users = str(tuple(users))
+		users.append(user)
+		users_str = str(tuple(users)).replace(',)',')')
 		conditions = f"owner in {users_str}" 
 	return conditions
 
-def has_permission(doc, user=None, permission_type=None):
-	users = get_employee_tree(parent=user)
-	users.append(user)
-	users_str = str(tuple(users)).replace(',)',')')
-
+def has_permission(doc, user, permission_type=None):
 	if frappe.db.get_value('Has Role',{'parent':user,'role':['in',['System Manager']]}):
 		# 如果角色包含管理员，则看到全量
-		conditions = ''
 		return True
 	else:
+		# 其他情况则只能看到自己,上级可以看到下级
 		users = get_employee_tree(parent=user)
+		users.append(user)
 		if doc.owner in users:
 			return True
 		else:
