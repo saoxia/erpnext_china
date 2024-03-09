e2e88be73707b3b2c2c3b6fd70c9267b9f0c860a
digitwise <407469895@qq.com>
2024-03-09 17:01:44 +0800

    Update oauth2_logins.py

--- erpnext_china/utils/oauth2_logins.py
+++ erpnext_china/utils/oauth2_logins.py
@@ -17,10 +17,10 @@ def login_via_wecom(code: str, state: str):
 	def get_userid(code,corpid,corpsecret):
 		access_token = get_access_token(corpid,corpsecret)
 		info = requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/auth/getuserinfo?access_token={access_token}&code={code}').json()
-		userid = info['userid']
-		user_info = requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={access_token}&userid={userid}').json()
-		info.update({'email':user_info['biz_mail']})
-		#info.update({'email':info['userid']})
+		#userid = info['userid']
+		#user_info = requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={access_token}&userid={userid}').json()
+		#info.update({'email':user_info['biz_mail']})
+		info.update({'email':info['userid']})
 		return info
 	
 	providers = frappe.get_all("Social Login Key", fields=["*"])
