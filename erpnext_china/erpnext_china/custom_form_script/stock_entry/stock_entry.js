59f701787b8feb7098e12c1d0fe5d88b72313abf
digitwise <407469895@qq.com>
2024-03-18 02:26:47 +0800

    手动移动库存时只能选择已定义的单位

---
+++ erpnext_china/erpnext_china/custom_form_script/stock_entry/stock_entry.js
@@ -0,0 +1,11 @@
+frappe.ui.form.on('Stock Entry', {
+	onload(frm) {
+		// 选择物料时，只选择已经定义的UOM
+		frm.fields_dict['items'].grid.get_field('uom').get_query = function(doc, cdt, cdn){
+			var row = locals[cdt][cdn];
+			return {
+				query:"erpnext.accounts.doctype.pricing_rule.pricing_rule.get_item_uoms",
+				filters: {'value':row.item_code, apply_on:"Item Code"},
+			}
+		};
+	}})
