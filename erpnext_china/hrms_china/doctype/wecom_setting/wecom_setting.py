ccbc2c19531d0eaaed8ceedd37544006ea9eaaec
digitwise <407469895@qq.com>
2024-03-30 16:19:04 +0800

    调整格式

--- erpnext_china/hrms_china/doctype/wecom_setting/wecom_setting.py
+++ erpnext_china/hrms_china/doctype/wecom_setting/wecom_setting.py
@@ -10,14 +10,8 @@ from erpnext_china.utils.oauth2_logins import get_access_token
 class WeComSetting(Document):
 	pass
 
-
-
-
 @frappe.whitelist(allow_guest=True)
 def update_access_token():
-	#client_id = frappe.db.get_single_value('WeCom Setting', 'client_id')
-	#client_secret = frappe.db.get_single_value('WeCom Setting', 'client_secret')
-	# access_token = get_access_token(client_id,client_secret)
 	client_info = frappe.db.get_singles_dict('WeCom Setting')
 	access_token = get_access_token(client_info['client_id'],client_info['client_secret'])
 
