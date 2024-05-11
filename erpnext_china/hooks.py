240f94cb496a102e0d8cf5590c20c062872cdff4
digitwise <407469895@qq.com>
2024-05-11 16:39:19 +0800

    Update hooks.py

--- erpnext_china/hooks.py
+++ erpnext_china/hooks.py
@@ -31,7 +31,7 @@ override_whitelisted_methods = {
 override_doctype_class = {
     'Social Login Key':'erpnext_china.hrms_china.custom_form_script.social_login_key.social_login_key.SocialLoginKey',
 	'Employee':'erpnext_china.hrms_china.custom_form_script.employee.employee.CustomEmployee',
-	'Lead':'erpnext_china.erpnext_china.custom_form_script.lead.laed.CustomLead',
+	'Lead':'erpnext_china.erpnext_china.custom_form_script.lead.lead.CustomLead',
 }
 
 doctype_js = {
