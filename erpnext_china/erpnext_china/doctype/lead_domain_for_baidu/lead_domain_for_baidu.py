9fd6e7a962afe051e8a31fc3b8fd3554d57e701e
ilqc <iliuchao2326@163.com>
2024-06-16 09:24:24 +0800

    refactor+feat: 1. format lead source 2.no username,crm lead name set anonymous

--- erpnext_china/erpnext_china/doctype/lead_domain_for_baidu/lead_domain_for_baidu.py
+++ erpnext_china/erpnext_china/doctype/lead_domain_for_baidu/lead_domain_for_baidu.py
@@ -48,10 +48,11 @@ def lead_via_baidu(**kwargs):
                 }
             )
             original_lead_doc = frappe.get_doc(kwargs).insert(ignore_permissions=True)
+            flow_channel_name = get_or_insert_flow_channel_name(kwargs.get('flow_channel_name', '其它'), '百度')
             # 同时生成一条CRM数据
-            crm_lead_doc = insert_or_get_crm_lead(
+            crm_lead_doc = get_or_insert_crm_lead(
                 username, 
-                '百度营销', 
+                flow_channel_name, 
                 kwargs.get('clue_phone_number'),
                 kwargs.get('clue_phone_number'), 
                 kwargs.get('wechat_account'),
@@ -68,14 +69,37 @@ def lead_via_baidu(**kwargs):
             
             update_delay_fields(record, kwargs)
             update_crm_lead_fields(record, kwargs)
-
+        return 'success'
     except Exception as e:
         frappe.db.rollback()
-        # 这里可以在企业微信里进行通知
-        # save_parse_failed_data('Original Leads', clue_id, kwargs, traceback.format_exc())
         # 抛出异常，将异常扔给百度，让他重试
         raise e
-    return 'success'
+    
+
+def get_or_insert_flow_channel_name(flow_channel_name: str, prefix: str):
+    # 首先判断是否已经有当前的流量渠道，没有则创建
+
+    flow_channel_name = format_flow_channel_name(flow_channel_name, prefix)
+    lead_source = get_doc_or_none('Lead Source', {
+        'source_name': flow_channel_name
+    })
+    if not lead_source:
+        lead_source_data = {
+            'doctype': 'Lead Source',
+            "source_name": flow_channel_name
+        }
+        lead_source = frappe.get_doc(lead_source_data).insert(ignore_permissions=True)
+    return lead_source.name
+
+
+def format_flow_channel_name(name: str, prefix: str):
+    # 有这种情况：搜索推广   百度搜索推广
+    # 字节-今日头条  其他渠道-手动导入
+    if name.startswith(prefix):
+        name = name.replace(prefix, '', 1)
+    if name.startswith('-'):
+        return prefix + name
+    return '-'.join([prefix, name])
 
 
 def update_delay_fields(record, kwargs):
@@ -87,20 +111,21 @@ def update_delay_fields(record, kwargs):
         original_user = frappe.session.user
         # 切换到具有足够权限的用户
         frappe.set_user('Administrator')
-        record.ad_uc_name = kwargs.get('ad_uc_name')
-        record.area_province = kwargs.get('area_province')
-        record.area = kwargs.get('area')
-        record.plan_id = kwargs.get('plan_id')
-        record.plan_name = kwargs.get('plan_name')
-        record.unit_id = kwargs.get('unit_id')
-        record.unit_name = kwargs.get('unit_name')
-        record.creative_id = kwargs.get('creative_id')
-        record.creative_name = kwargs.get('creative_name')
-        record.keyword_id = kwargs.get('keyword_id')
-        record.keyword = kwargs.get('keyword')
-        record.search_word = kwargs.get('search_word')
-        record.refund_supportive = kwargs.get('refund_supportive')
-        
+        # 为了防止 字段没有值的请求将已经存在的字段值覆盖
+        # 定义需要更新的字段列表
+        fields_to_update = [
+            'ad_uc_name', 'area_province', 'area', 'plan_id', 
+            'plan_name', 'unit_id', 'unit_name', 'creative_id', 
+            'creative_name', 'keyword_id', 'keyword', 'search_word', 
+            'refund_supportive'
+        ]
+
+        # 使用字典推导和循环更新记录中的字段
+        for field in fields_to_update:
+            if kwargs.get(field):
+                setattr(record, field, kwargs.get(field))
+
+        # 保存记录
         record.save()
         # 切换回原来的用户
         frappe.set_user(original_user)
@@ -121,8 +146,10 @@ def update_crm_lead_fields(record, kwargs):
             original_user = frappe.session.user
             # 切换到具有足够权限的用户
             frappe.set_user('Administrator')
-            crm_lead.city = kwargs.get('area')
-            crm_lead.state = kwargs.get('area_province')
+            if kwargs.get('area'):
+                crm_lead.city = kwargs.get('area')
+            if kwargs.get('area_province'):
+                crm_lead.state = kwargs.get('area_province')
             crm_lead.save()
             # 切换回原来的用户
             frappe.set_user(original_user)
@@ -162,7 +189,7 @@ def get_username_in_form_detail(kwargs: dict, source: str):
     """
     if source == 'baidu':
         form_detail = kwargs.get('form_detail')
-        username = kwargs.get('clue_id')
+        username = "匿名"
         # 目前只有solution_type_name是表单的来源才有称呼
         if '表单' in kwargs.get('solution_type_name', '') and form_detail:
             if isinstance(form_detail, str):
@@ -174,46 +201,46 @@ def get_username_in_form_detail(kwargs: dict, source: str):
                         break
         return username
 
-    return kwargs.get('name') or kwargs.get('id')
+    return kwargs.get('name', '匿名') or "匿名"
 
 
-def insert_or_get_crm_lead(lead_name, source, phone, mobile, wx, city, state, country='China'):
+def get_or_insert_crm_lead(lead_name, source, phone, mobile, wx, city, state, country='China'):
     """
     通过手机、电话、微信号查找线索是否已经存在
     """
     # 如果phone、mobile、wx都没有，则不需要创建CRM线索
-    if not phone and not mobile and not wx:
+    if not any([phone, mobile, wx]):
         return None
+
+    or_filters = {}
+    for field, value in [('phone', phone), ('mobile_no', mobile), ('custom_wechat', wx)]:
+        if value:
+            or_filters[field] = value
+    
+    # 检查是否存在匹配的记录
+    records = frappe.get_list(
+        "Lead",
+        or_filters=or_filters,
+        ignore_permissions=True
+    )
+    if len(records) > 0:
+        record = records[0]
     else:
-        # 查询已经存在的CRM线索
-        or_filters = {}
-        if phone: or_filters.update({'phone': phone})
-        if mobile: or_filters.update({'mobile_no': mobile})
-        if wx: or_filters.update({'custom_wechat': wx})
-
-        records = frappe.get_list(
-            "Lead",
-            or_filters=or_filters,
-            ignore_permissions=True
-        )
-
-        # 如果CRM线索中不存在当前手机号/电话号/微信号的线索，则新增
-        if len(records) == 0:
-            crm_lead_data = {
-                'doctype': 'Lead',
-                'lead_name': lead_name,
-                'source': source,
-                'phone': phone,
-                'mobile_no': mobile,
-                'custom_wechat': wx,
-                'city': city,
-                'state': state,
-                'country': country,
-            }
-            record = frappe.get_doc(crm_lead_data).insert(ignore_permissions=True)
-        else: # 如果存在，则返回第一个
-            record = records[0]
-        return record
+        # 构造新记录的数据
+        crm_lead_data = {
+            'doctype': 'Lead',
+            'lead_name': lead_name,
+            'source': source,
+            'phone': phone or '',
+            'mobile_no': mobile or '',
+            'custom_wechat': wx or '',
+            'city': city,
+            'state': state,
+            'country': country,
+        }
+        # 插入新记录
+        record = frappe.get_doc(crm_lead_data).insert(ignore_permissions=True)
+    return record
 
 
 @frappe.whitelist(allow_guest=True)
@@ -252,11 +279,11 @@ def lead_via_douyin(**kwargs):
                 }
             )
             original_lead_doc = frappe.get_doc(kwargs).insert(ignore_permissions=True)
-            
+            clue_source = get_or_insert_flow_channel_name(kwargs.get('clue_source', '其它'))
             # 同时生成一条CRM数据
-            crm_lead_doc = insert_or_get_crm_lead(
+            crm_lead_doc = get_or_insert_crm_lead(
                 username, 
-                '抖音广告', 
+                clue_source, 
                 kwargs.get('telphone'),
                 kwargs.get('telphone'), 
                 kwargs.get('weixin'), 
@@ -264,11 +291,13 @@ def lead_via_douyin(**kwargs):
                 kwargs.get('province_name'))
             
             # 添加crm 线索和原始线索之间的关系
-            original_lead_doc.crm_lead = crm_lead_doc.name
-            original_lead_doc.save()
+            if crm_lead_doc:
+                original_lead_doc.crm_lead = crm_lead_doc.name
+                original_lead_doc.save()
+        
+        return  {'code': 0,'message': 'success'}
     except Exception as e:
         # 如果出现异常，回滚之前的操作
         frappe.db.rollback()
         raise e
-        # save_parse_failed_data('Original Leads', clue_id, kwargs, traceback.format_exc())
-    return  {'code': 0,'message': 'success'}
+
