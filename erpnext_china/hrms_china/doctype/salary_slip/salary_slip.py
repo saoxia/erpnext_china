38255dda9668555b8bbaf4133d8295a8abf9ee71
ilqc <iliuchao2326@163.com>
2024-09-20 11:16:29 +0800

    feat: salary detail total amount

--- erpnext_china/hrms_china/doctype/salary_slip/salary_slip.py
+++ erpnext_china/hrms_china/doctype/salary_slip/salary_slip.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2024, Digitwise Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SalarySlip(Document):
-	pass
+	
+
+    @property
+    def total_amount(self):
+        total = 0
+        for i in self.salary_detail:
+            doc = frappe.get_doc('Salary Component', i.component)
+            if doc.type == '收入':
+                total += i.amount
+            else:
+                total -= i.amount
+        print(total)
+        return total
